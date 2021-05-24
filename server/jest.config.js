10b5714e31b3dd282df660f3d602bdd5f2b243e7
Lucas dev <devlucasmarques@gmail.com>
2021-05-24 19:55:29 -0300

    test ajusts

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   roots: [
-    "<rootDir>/src"
+    "<rootDir>/test"
   ],
   testEnvironment: "node",
   transform: {
--- server/test/controllers/indexController.test.ts
+++ server/test/controllers/indexController.test.ts
@@ -2,10 +2,25 @@ import request from 'supertest';
 import { app } from '../../src/app';
 
 describe('Test IndexController', () => {
-  it('Should return response index"!', async () => {
+  it('Should return status 200 and "Hello World"!', async () => {
     const result = await request(app).get('/').send();
 
     expect(result.status).toBe(200);
-    expect(result.body.data).toBe('Hello World');
+    expect(result.text).toBe('Hello World');
+  });
+
+  it('Should return 404"!', async () => {
+    const result = await request(app).get('/404').send();
+
+    expect(result.status).toBe(404);
+  });
+
+  it('Should return exception', async () => {
+    await request(app)
+      .get('/oi')
+      .catch((res) => {
+        expect(res.status).toBe(404);
+        expect(res.text).toBe('Hello World');
+      });
   });
 });
